0486da3be746a30cbc99363c89725cb1422018eb
Alex Innes <alexnixoninnes@gmail.com>
2018-10-18 15:12:52 +0100

    Added a pester test file.

---
+++ SystemUptime.Tests.ps1
@@ -0,0 +1,7 @@
+#This is a script to test that the SystemUptime Script is still functioning as intended.
+
+$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+#write some test
